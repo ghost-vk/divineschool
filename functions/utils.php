ecb824a7e2e566c58f305b136f2fe152482c2cf2
ghost <ghost.vkv@gmail.com>
2021-04-06 21:41:26 +0300

    Fixed loader position in popup buttons. Fixed usage constant in phpmailer init function.

--- functions/utils.php
+++ functions/utils.php
@@ -17,13 +17,15 @@ use PHPMailer\PHPMailer\PHPMailer;
  */
 add_action( 'phpmailer_init', 'send_smtp_email' );
 function send_smtp_email( PHPMailer $phpmailer ) {
-	$phpmailer->isSMTP();
-	$phpmailer->Host       = SMTP_HOST;
-	$phpmailer->SMTPAuth   = SMTP_AUTH;
-	$phpmailer->Port       = SMTP_PORT;
-	$phpmailer->Username   = SMTP_USER;
-	$phpmailer->Password   = SMTP_PASS;
-	$phpmailer->SMTPSecure = SMTP_SECURE;
-	$phpmailer->From       = SMTP_FROM;
-	$phpmailer->FromName   = SMTP_NAME;
+	if ( defined('SMTP_USER') && defined('SMTP_PASS') ) {
+		$phpmailer->isSMTP();
+		$phpmailer->Host       = SMTP_HOST;
+		$phpmailer->SMTPAuth   = SMTP_AUTH;
+		$phpmailer->Port       = SMTP_PORT;
+		$phpmailer->Username   = SMTP_USER;
+		$phpmailer->Password   = SMTP_PASS;
+		$phpmailer->SMTPSecure = SMTP_SECURE;
+		$phpmailer->From       = SMTP_FROM;
+		$phpmailer->FromName   = SMTP_NAME;
+	}
 }
