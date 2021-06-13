f0924c3bdad2488471aa4cc54f1f4ce5027c7655
ghost <ghost.vkv@gmail.com>
2021-06-13 13:16:17 +0300

    Add analytics

---
+++ js/analytics/index.js
@@ -0,0 +1,28 @@
+/**
+ * Facebook Pixel
+ */
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '289404779547933');
+fbq('track', 'PageView');
+
+
+/**
+ * Yandex.Metrika
+ */
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(80601841, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true
+});
+
